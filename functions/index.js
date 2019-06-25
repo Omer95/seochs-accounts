ecc722e7c37bbd208dd1df7a71a01c75c7555c0e
Mirza. Beg <omerfarooqali@gmail.com>
2019-06-25 23:34:18 +0500

    progress in cloud functions cron job

--- functions/index.js
+++ functions/index.js
@@ -15,11 +15,16 @@ var globVal = 2000;
 exports.scheduledJob = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
     console.log('Hello');
     const db = admin.database();
-    return db.ref('/plots/{}/debits').push({
-        debit: "security fees",
-        value: globVal,
-        date: new Date()
-    }).catch(err => {
-        console.log(err);
+    return db.ref('/plots').once('value').then(snap=> {
+        Object.keys(snap.val()).forEach(plot=> {
+            console.log(plot);
+            return db.ref(`/plots/${plot}/debits`).push({
+                description: 'security fees',
+                debitAmount: 2000,
+                date: new Date()
+            });
+        });
+    }).catch(error => {
+        console.log(error);
     });
-})
+});
